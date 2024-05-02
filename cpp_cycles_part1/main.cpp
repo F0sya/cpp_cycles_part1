294412878817e97a6e2f1fb8af9e1310d54eba1b
F0sya <nikitachmyria@gmail.com>
2024-05-02 19:51:00 +0300

    The fourth additional exercise is done.

--- cpp_cycles_part1/main.cpp
+++ cpp_cycles_part1/main.cpp
@@ -2,21 +2,13 @@
 using namespace std;
 
 int main() {
-	int n; cout << "Enter a number:"; cin >> n;
-	int res = 1;
-
-	if (n < 0) {
-		cout << "Wrong input!" << endl;
-		return 0;
+	int L; cout << "Enter a length of cloth:"; cin >> L;
+	int R; cout << "Enter a length of pillow:"; cin >> R;
+	int res = 0;
+	while (L != 0) {
+		L -= R;
+		res++;
 	}
-	else if (n == 0) {
-		cout << "Factorial:" << 1 << endl;
-		return 0;
-	}
-	
-	while (n != 0) {
-		res *= n;
-		n--;
-	}
-	cout << "Factorial:" << res << endl;
+	cout << "Amount of pillows:" << res << endl;
+	return 0;
 }
