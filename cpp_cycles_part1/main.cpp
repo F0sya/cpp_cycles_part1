f8402895ede458e80cc1147101ba77202e9fea8e
F0sya <nikitachmyria@gmail.com>
2024-05-02 19:44:24 +0300

    The additional exercise 2 is done.

--- cpp_cycles_part1/main.cpp
+++ cpp_cycles_part1/main.cpp
@@ -2,26 +2,16 @@
 using namespace std;
 
 int main() {
-	char k; cout << "Enter a symbol:"; cin >> k;
-	int n; cout << "Enter an amount of symbols:"; cin >> n;
-	int axis; cout << "Horizontal(1) or Vertical(2)?"; cin >> axis;
-	if (axis == 1) {
-		while (n != 0) {
-			cout << k;
-			n--;
+	int left; cout << "Enter left limit:"; cin >> left;
+	int right; cout << "Enter right limit:"; cin >> right;
+	int sum = 0;
+	
+	while (left != right + 1) {
+		if (left % 2 != 0) {
+			sum += left;
 		}
-		cout << endl;
-		return 0;
+		left++;
 	}
-	else if (axis == 2) {
-		while (n != 0) {
-			cout << k << endl;
-			n--;
-		}
-		return 0;
-	}
-	else {
-		cout << "Wrong input" << endl;
-		return 0;
-	}
+	cout << "Result:" << sum << endl;
+	return 0;
 }
