cf45fbe5456d78ca8c296b55b5c7c38fe51d568e
F0sya <nikitachmyria@gmail.com>
2024-05-02 18:42:31 +0300

    The third practice exercise is done

---
+++ cpp_cycles_part1/main.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+	int left; cout << "Enter left limit:"; cin >> left;
+	int right; cout << "Enter right limit:"; cin >> right;
+	int sum = 0;
+	while (left != right + 1) {
+		sum += left;
+		left++;
+	}
+	cout << "Result:" << sum << endl;
+	return 0;
+}
