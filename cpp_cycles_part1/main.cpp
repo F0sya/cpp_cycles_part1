3fc17378c499f4bade2c704ee04b7b187ec7d622
F0sya <nikitachmyria@gmail.com>
2024-05-02 20:00:35 +0300

    The fifth additional exercise is done

--- cpp_cycles_part1/main.cpp
+++ cpp_cycles_part1/main.cpp
@@ -2,13 +2,17 @@
 using namespace std;
 
 int main() {
-	int L; cout << "Enter a length of cloth:"; cin >> L;
-	int R; cout << "Enter a length of pillow:"; cin >> R;
+	int N; cout << "Enter an amount of boxes with apples:"; cin >> N;
+	int amount; cout << "Enter an amount of boxes that cars can take:"; cin >> amount;
 	int res = 0;
-	while (L != 0) {
-		L -= R;
+	if (N < 0) {
+		cout << "Wrong input!" << endl;
+		return 0;
+	}
+	while (N > 0) {
+		N -= amount;
 		res++;
 	}
-	cout << "Amount of pillows:" << res << endl;
+	cout << "Amount of cars:" << res << endl;
 	return 0;
 }
