2f8e89e3b0d5fda68fabe75aedc3877e87dd076c
F0sya <nikitachmyria@gmail.com>
2024-05-02 19:47:45 +0300

    The additional exercise 3 is done.

--- cpp_cycles_part1/main.cpp
+++ cpp_cycles_part1/main.cpp
@@ -2,16 +2,21 @@
 using namespace std;
 
 int main() {
-	int left; cout << "Enter left limit:"; cin >> left;
-	int right; cout << "Enter right limit:"; cin >> right;
-	int sum = 0;
-	
-	while (left != right + 1) {
-		if (left % 2 != 0) {
-			sum += left;
-		}
-		left++;
+	int n; cout << "Enter a number:"; cin >> n;
+	int res = 1;
+
+	if (n < 0) {
+		cout << "Wrong input!" << endl;
+		return 0;
 	}
-	cout << "Result:" << sum << endl;
-	return 0;
+	else if (n == 0) {
+		cout << "Factorial:" << 1 << endl;
+		return 0;
+	}
+	
+	while (n != 0) {
+		res *= n;
+		n--;
+	}
+	cout << "Factorial:" << res << endl;
 }
